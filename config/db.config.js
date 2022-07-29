1418788fdfd8f24211558b9571f8993744bff28f
Rene0115 <enere0115@gmail.com>
2022-07-29 10:12:01 +0100

    database connect and middleware setups

---
+++ config/db.config.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { logger } from '../app.js';
+
+dotenv.config();
+
+const database = () => {
+  mongoose.connect(process.env.MONGODB_URI).then((value) => logger.info('database connected')).catch((err) => logger.info(err));
+};
+
+export default database;
