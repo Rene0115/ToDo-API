83f5ec40174103939638db9f03eacc9e18c880b8
Rene0115 <enere0115@gmail.com>
2022-07-29 10:28:45 +0100

    setup

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import pino from 'pino';
+import middleware from './middlewares/middlewares';
 
 const app = express();
 const logger = pino();
 
+middleware(app);
+
 const port = 5000;
 app.listen(port, ()=>{
     logger.info(`Server is running on port ${port}`);
-})
+});
--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,12 +1,19 @@
 import router from "../routes/index.routes.js";
 import express from 'express';
 import bodyParser from "body-parser";
-
+import database from "../config/db.config.js";
+import errorHandler from './error.middleware.js';
 const middleware = (app) =>{
     app.use(express.urlencoded({ extended: true}));
     app.use(express.json());
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(cors());
-    
+    database();
+    app.use('*', (req, res) =>{
+        res.status(200).send('Server is Running Check API docs')
+    });
+    app.use(errorHandler);
 
-}
+};
+
+export default middleware;
