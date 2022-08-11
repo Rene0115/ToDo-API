d4e37724deb4d8f64016312f19b78cc74b21e44d
Rene0115 <enere0115@gmail.com>
2022-08-12 00:56:50 +0100

    validators added

---
+++ validators/list.validator.js
@@ -0,0 +1,20 @@
+import Joi from 'joi';
+
+class ListValidator {
+  createListSchema = Joi.object({
+    title: Joi.string().required(),
+    content: Joi.string().required(),
+    userId: Joi.string().required()
+  });
+
+  updateListSchema = Joi.object({
+    id: Joi.string().required(),
+    content: Joi.string(),
+    title: Joi.string()
+  });
+
+  deleteListSchema = Joi.object({
+    id: Joi.string().required()
+  });
+}
+export default new ListValidator();
