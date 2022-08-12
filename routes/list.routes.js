78874826d1970c0d33507ebb143be04d2dcbdc8e
Rene0115 <enere0115@gmail.com>
2022-08-13 00:17:22 +0100

    setup update and create routes

--- routes/list.routes.js
+++ routes/list.routes.js
@@ -1,8 +1,11 @@
 /* eslint-disable import/extensions */
 import express from 'express';
 import listController from '../controllers/list.controller.js';
+import validator from '../validators/validator.js';
+import authentication from '../middlewares/auth.middleware.js';
+import listValidator from '../validators/list.validator.js';
 
 const listRouter = express.Router();
-listRouter.post('/', listController.createList);
-
+listRouter.post('/', [validator(listValidator.createListSchema), authentication], listController.createList);
+listRouter.put('/', [validator(listValidator.updateListSchema), authentication], listController.updateList);
 export default listRouter;
