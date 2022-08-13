21c6fcba5b9f3e3853758f21bdfb0d66814c2a75
Rene0115 <enere0115@gmail.com>
2022-08-13 17:24:39 +0100

    improved create user feature

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,22 +7,30 @@ import userService from '../services/user.service.js';
 
 class UserController {
   async createUser(req, res) {
+    const user = await userService.findByEmail(req.body);
+    if (!_.isEmpty(user)) {
+      return res.status(400).send({
+        success: false,
+        message: 'User already exists'
+      });
+    }
     const data = { email: req.body.email, password: bcrypt.hashSync(req.body.password, 8) };
-    await userService.create(data);
     if (_.isEmpty(data)) {
       return res.status(404).send({
         success: false,
         message: 'User cannot be created without an email and a password'
       });
     }
+    await userService.create(data);
 
     return res.status(201).send({
       success: true,
       message: 'user created successfully'
     });
   }
+
   async loginUser(req, res) {
-    
+
   }
 }
 
