a4d449af475f45d9a1819c9c2958a765c3bceb36
Rene0115 <enere0115@gmail.com>
2022-08-13 17:43:09 +0100

    created user routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,11 @@
 /* eslint-disable import/extensions */
 import express from 'express';
 import listRouter from './list.routes.js';
+import userRouter from './user.routes.js';
 
 const router = express.Router();
 
 router.use('/lists', listRouter);
+router.use('/users', userRouter);
 
 export default router;
---
+++ routes/user.routes.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/extensions */
+import express from 'express';
+import userController from '../controllers/user.controller.js';
+import validateUserSchema from '../validators/user.validator.js';
+import validator from '../validators/validator.js';
+
+const userRouter = express.Router();
+
+userRouter.post('/signup', [validator(validateUserSchema)], userController.createUser);
+userRouter.post('/login', [validator(validateUserSchema)], userController.loginUser);
+userRouter.get('/getUsers', userController.fetchUsers);
+
+export default userRouter;
