757164aef86b0af618bc8fbe5cceecd094bbb9cb
Rene0115 <enere0115@gmail.com>
2022-08-14 14:14:07 +0100

    removed comments in error middleware

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -4,9 +4,7 @@ import pino from 'pino';
 const logger = pino();
 
 const errorHandler = (err, req, res, next) => {
-//   if (process.env.NODE_ENV !== 'production') {
   logger.error(err);
-  //   }
   return res.status(500).send({ success: false, message: err.message });
 };
 
