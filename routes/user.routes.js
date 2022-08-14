d774566820c15661a9ffca33596ca2f892eec18f
Rene0115 <enere0115@gmail.com>
2022-08-14 22:01:51 +0100

    get all users route added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,6 +6,7 @@ import validator from '../validators/validator.js';
 
 const userRouter = express.Router();
 
+userRouter.get('/', userController.fetchUsers);
 userRouter.post('/signup', [validator(validateUserSchema)], userController.createUser);
 userRouter.post('/login', [validator(validateUserSchema)], userController.loginUser);
 userRouter.get('/getUsers', userController.fetchUsers);
